9cc53fb63a67a81fbf0833ee06927de3731b96bb
君島克弥 <K.Kimishima123@gmail.com>
2017-08-06 19:38:34 +0900

    じゃんけんゲーム練習

---
+++ 3.janken/src/list3-9-1.c
@@ -0,0 +1,32 @@
+// じゃんけんゲーム改良
+// 自分なりにソースを書く
+//
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+//定数
+#define PATTEN_MIN 0  // じゃんけんの表示最小
+#define PATTEN_MAX 3  // じゃんけんの表示最大
+
+// 構造体
+typedef struct{       // じゃんけん入力構造体
+  int input;
+  int random;
+}junken_value;
+
+typedef struct{       // じゃんけん結果
+  int win;
+  int lose;
+  int draw;
+}junken_result;
+
+void (*func[])
+
+// メイン関数
+int main(){
+  
+
+  return 0;
+}
