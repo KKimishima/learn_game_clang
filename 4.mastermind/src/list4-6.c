7de9fca464a6d635d332baa4116af6c7b49ef254
君島克弥 <K.Kimishima123@gmail.com>
2017-08-14 19:38:25 +0900

    一個前のコミットに戻る

--- 4.mastermind/src/list4-6.c
+++ 4.mastermind/src/list4-6.c
@@ -21,50 +21,61 @@
 #include<ctype.h>
 #include<time.h>
 
-#define NUBER_MAX 9
-#define DIGITS 4  // 出題数列の最大
+#define NUBER_MAX 10  // 生成されるランダム数の限界(9だが+1)
+#define DIGITS 4      // 出題数列の最大
 
 //typedef struct{
-//  int random_num[DIGITS];
-//  typedef 
-//}
+//  int   random_num[DIGITS];
+  //  input_str
+//} game_value;
+
+//typedef struct{
+//  int   input_num[DIGITS];
+//  char  input_str[DIGITS + 2]; 
+//} input_value;
 // ゲームの判定関数
 
+//void  value_init(game_value *values);
+//void  input_init(input_value *inputs);
+//void  random_init(game_value *values);
+//void  input_interface(input_value *inputs);
+//int   input_func(input_value *inputs);
 
-//数値を入力
-int input_func(){
-  int   input_num[DIGITS] = {0,0,0,0};
-  char  input_str[DIGITS + 2];                  // 文字列のため+1と必ず改行コードを入れるため+1
+  //数値を入力
+int input_func(input_value *inputs){
   int   i,j;
+  // 初期化
+//  input_init(&inputs);
 
   puts("四桁の数値を入力してください");
   puts("四桁以上の文字は無効になります!!!");
   printf("数値:");
-  fgets(input_str,sizeof(input_str) + 1,stdin); // 文字オーバを収納するため+1
+  fgets(inputs->input_str,sizeof(inputs->input_str) + 1,stdin); // 文字オーバを収納するため+1
  
   // 文字数が少ない場合
-  if(strlen(input_str) <= 4){  //
+  if(strlen(inputs->input_str) <= 4){  //
     return 1;
    }
 
   // 文字数が多い場合
-  if(strlen(input_str) > 5){  //
+  if(strlen(inputs ->input_str) > 5){  //
     while ( getchar()  !=  '\n'  );             // 入力バッファから,\nまで読み飛ばし
     return 2;
   }
   
   // 文字が含まれいないか確認
   for(i = 0;i < DIGITS;i++){
-    if(!isdigit(input_str[i])){                 // 文字が含まれるとfalse(0)がかえされる
+    if(!isdigit(inputs->input_str[i])){                 // 文字が含まれるとfalse(0)がかえされる
       return 3;
     }
   }
 
   // 同じ文字があるか確認
   for(i = 0;i < DIGITS;i++){
-    input_num[i] = input_str[i] - '0';          // `0`を引いて数字キャストする
+    inputs->input_num[i] = inputs->input_str[i]  - '0';
+ //   input_num[i] = input_str[i] - '0';          // `0`を引いて数字キャストする
     for(j = 0;j< i ;j++){
-      if(input_str[i] == input_str[j]){
+      if(inputs->input_str[i] == inputs->input_str[j]){
         return 4;
       }
     }
@@ -73,11 +84,11 @@ int input_func(){
   return 0;
 }
 
-// インプットチェック関数
-void inputcheck_func(){
+// 入力インターフェイス関数
+void input_interface(input_value *inputs){
   int input_frag;
   do{
-    input_frag =  input_func();
+    input_frag =  input_func(&inputs);
     switch(input_frag){
       case  0:  puts("***OK***");                       break;
       case  1:  puts("***文字数が少なすぎ***");         break;
@@ -90,38 +101,32 @@ void inputcheck_func(){
   return;
 }
 
-// ランダム生成の初期化
-void random_init(){
+// ランダム生成の関数
+void random_init(int random_num[]){
   int i,j;
-  int random_test[DIGITS];
-  int random_temp;
+  int random_comp;            // 乱数の比較用変数
+
   puts("ランダム数値の生成");
   
   // 重複のないランダムな数値の入力
-  for(i = 0;i <= sizeof(random_test) / sizeof(random_test[0]);i++){ // 一重目のループ:配列の数だけ繰り返す
+  for(i = 0;i <= sizeof(random_num) / sizeofa(random_num[0]);i++){ // 一重目のループ:配列の数だけ繰り返す
     do{                                                             // 二重目のループ:基本配列数と検証入れうすう
-      random_temp = 0 + rand() % NUBER_MAX + 1;                     // 0~9までの乱数を比較用一時変数に収める+1する
+      random_comp = 0 + rand() % NUBER_MAX;                         // 0~9までの乱数を比較用一時変数に収める
       for(j = 0;j < i;j++){                                         // 三重ループ:0(j)かカレント配列数(i)まで
-        if(random_test[j] == random_temp){                          // すでにデータが得られているか調べる
+        if(values->random_num[j] == random_comp){                          // すでにデータが得られているか調べる
           break;                                                    // ブレイクするとwhileの条件で必ず弾かれるので
         }                                                           // 正常な数値が入れまで繰り返される
       }
-    }while(i < j );
-    random_test[i] = random_temp;
-    printf("%d",random_test[i]);
+    }while(j < i);
+    values->random_num[i] = random_comp;
   }
-  printf("\n");
   return ;
 }
 
-// 初期化関数
-void value_init(){
-  return;
-}
 
 int main(){
-  int random_num;
-  int ans_num;
+  int i;                                  // デバッグ用
+  int   random_num[DIGITS];
   int retry;
   time_t start,end;
 
@@ -129,11 +134,17 @@ int main(){
 
   retry = 0;
   do{
-    value_init();
+    //  ランダム生成関数
+    random_init(random_num);                 
 
-    random_init();
- 
-    inputcheck_func();
+    puts("ランダムテスト");               // デバッグ用
+    for(i = 0;i < DIGITS;i++){            // デバッグ用
+      printf("%d",values.random_num[i]);  // デバッグ用
+    }                                     // デバッグ用
+    puts("");                             // デバッグ用
+
+    // 入力インターフェイス関数
+    input_interface(&inputs);
 
 //    hitblow_func();
   }while(retry == 1);
